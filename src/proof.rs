e1107fe301d73643e9bcac511b4cb9df7c30afa1
Jan Winkelmann (keks) <jan@cryspen.com>
2025-11-01 13:08:30 +0100

    Dual-license the code as Apache-2.0 and MIT.
    - Adds SPDX headers
    - Adds LICENSE files
    - Adds SPDX info to Cargo.toml

--- src/proof.rs
+++ src/proof.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::{expressions::Expression, gamehops::GameHop, theorem::GameInstance};
