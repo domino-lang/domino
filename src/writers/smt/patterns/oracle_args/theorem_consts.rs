2fa4c281a8f87423eb3e5a0f6ad42b3336e0518e
Jan Winkelmann <146678+keks@users.noreply.github.com>
2025-11-05 14:46:18 +0100

    Dual-license the code as Apache-2.0 and MIT (#125)
    - Adds SPDX headers
    - Adds LICENSE files
    - Adds SPDX info to Cargo.toml

--- src/writers/smt/patterns/oracle_args/theorem_consts.rs
+++ src/writers/smt/patterns/oracle_args/theorem_consts.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use crate::writers::smt::patterns::{
     theorem_consts::TheoremConstsPattern as TheoremConstsDatatypePattern, DatastructurePattern as _,
 };
