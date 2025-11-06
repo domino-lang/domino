sspverif/ssbee

start of python generator

src/project/mod.rs
@@ -101,6 +101,7 @@ impl Files {
 #[derive(Debug)]
 pub struct Project<'a> {
     root_dir: PathBuf,
+    packages: HashMap<String, Package>,
     games: HashMap<String, Composition>,
     proofs: HashMap<String, Proof<'a>>,
 }
@@ -110,6 +111,7 @@ impl<'a> Project<'a> {
     pub(crate) fn empty() -> Self {
         Self {
             root_dir: PathBuf::new(),
+            packages: HashMap::new(),
             games: HashMap::new(),
             proofs: HashMap::new(),
         }
@@ -149,6 +151,7 @@ impl<'a> Project<'a> {
         let proofs = load::proofs(&files.proofs, packages.to_owned(), games.to_owned())?;
 
         let project = Project {
+            packages,
             root_dir,
             games,
             proofs,
@@ -261,6 +264,30 @@ impl<'a> Project<'a> {
             }
 
             ui.finish_proof(&proof.name);
+        }
+
+        Ok(())
+    }
+
+    pub fn python(&self) -> Result<()> {
+        let mut path = self.root_dir.clone();
+        path.push("_build/python/");
+        std::fs::create_dir_all(&path)?;
+
+        println!("from dataclasses import dataclass");
+
+        for (name, pkg) in &self.packages {
+            use crate::writers::python::patterns::game_state::*;
+
+            println!("{}", DataclassWriter(PackageStatePattern::new(pkg)));
+        }
+
+        for (name, proof) in &self.proofs {
+            for game_inst in &proof.instances {
+                use crate::writers::python::patterns::game_state::*;
+
+                println!("{}", DataclassWriter(GameStatePattern::new(game_inst)));
+            }
         }
 
         Ok(())

src/writers/python/patterns/game_state.rs
@@ -1,0 +1,165 @@
+/*
+*
+*
+* We need to
+* - figure out the types to use on python's side
+*   - bits_n
+*     - BitVector? external dep.
+*     - bitvec? same problem.
+*     - maybe we just need deps
+*   - int -> int
+*   - bool -> bool
+*
+* - we need the const assignments in here - maybe simply full gameinstance?
+* - what sample info do we need?
+*   - here, not so much, but we need to find a way to do randomness mapping
+*
+* */
+
+use std::fmt::Display;
+
+use crate::{package::Package, proof::GameInstance};
+
+static UNIT: () = ();
+
+pub struct GameStatePattern<'a> {
+    game_inst: &'a GameInstance,
+}
+
+impl<'a> GameStatePattern<'a> {
+    pub fn new(game_inst: &'a GameInstance) -> Self {
+        Self { game_inst }
+    }
+}
+
+pub struct PackageStatePattern<'a> {
+    pkg: &'a Package,
+}
+
+impl<'a> PackageStatePattern<'a> {
+    pub fn new(pkg: &'a Package) -> Self {
+        Self { pkg }
+    }
+}
+
+struct GameStateTypeName<'a>(&'a str);
+enum GameStateFieldName<'a> {
+    PackageState(&'a str),
+    Randomness(&'a str),
+}
+struct PackageStateTypeName<'a>(&'a str);
+struct PackageStateFieldName<'a>(&'a str);
+
+impl<'a> Display for GameStateTypeName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self(name) = self;
+        write!(f, "GameState_{name}")
+    }
+}
+
+impl<'a> Display for GameStateFieldName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GameStateFieldName::PackageState(name) => write!(f, "pkg_{name}"),
+            GameStateFieldName::Randomness(_) => todo!(),
+        }
+    }
+}
+
+impl<'a> Display for PackageStateTypeName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self(name) = self;
+        write!(f, "PackageState_{name}")
+    }
+}
+
+impl<'a> Display for PackageStateFieldName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self(name) = self;
+        write!(f, "{name}")
+    }
+}
+
+impl<'a> Dataclass for PackageStatePattern<'a> {
+    fn name(&self) -> impl Display {
+        PackageStateTypeName(self.pkg.name())
+    }
+
+    fn fields(&self) -> impl IntoIterator<Item = (impl Display, PyType<'_>)> {
+        self.pkg.state.iter().map(|(name, ty, _)| {
+            (
+                PackageStateFieldName(name.as_str()),
+                ty.clone().try_into().unwrap(),
+            )
+        })
+    }
+}
+
+impl<'a> Dataclass for GameStatePattern<'a> {
+    fn name(&self) -> impl Display {
+        self.game_inst.game().name()
+    }
+
+    fn fields(&self) -> impl IntoIterator<Item = (impl Display, PyType)> {
+        self.game_inst.game.pkgs.iter().map(|pkg_inst| {
+            (
+                GameStateFieldName::PackageState(pkg_inst.name()),
+                PyType::PackageState(pkg_inst.pkg_name()),
+            )
+        })
+    }
+}
+
+pub struct DataclassWriter<DC: Dataclass>(pub DC);
+
+impl<DC: Dataclass> Display for DataclassWriter<DC> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "@dataclass")?;
+        writeln!(f, "class {}:", self.0.name())?;
+        for (name, ty) in self.0.fields() {
+            writeln!(f, "  {name}: {ty}")?;
+        }
+        Ok(())
+    }
+}
+
+struct BitVecLength<'a>(&'a ());
+
+enum PyType<'a> {
+    BitVec(BitVecLength<'a>),
+    Int,
+    Bool,
+    List(Box<Self>),
+    PackageState(&'a str),
+}
+
+impl<'a> Display for PyType<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PyType::BitVec(bit_vec_length) => todo!(),
+            PyType::Int => write!(f, "int"),
+            PyType::Bool => write!(f, "bool"),
+            PyType::List(py_type) => write!(f, "List[{py_type}]"),
+            PyType::PackageState(name) => write!(f, "{}", PackageStateTypeName(name)),
+        }
+    }
+}
+
+impl<'a> TryFrom<crate::types::Type> for PyType<'a> {
+    type Error = ();
+
+    fn try_from(value: crate::types::Type) -> Result<Self, Self::Error> {
+        match value {
+            crate::types::Type::Integer => Ok(PyType::Int),
+            crate::types::Type::Boolean => Ok(PyType::Bool),
+            crate::types::Type::Bits(_count_spec) => Ok(PyType::BitVec(BitVecLength(&UNIT))),
+            crate::types::Type::List(ty) => PyType::try_from(*ty).map(Box::new).map(PyType::List),
+            _ => todo!(),
+        }
+    }
+}
+
+trait Dataclass {
+    fn name(&self) -> impl Display;
+    fn fields(&self) -> impl IntoIterator<Item = (impl Display, PyType)>;
+}

domino-lang/domino

src/project/mod.rs
@@ -103,6 +103,7 @@ impl Files {
 #[derive(Debug)]
 pub struct Project<'a> {
     root_dir: PathBuf,
+    packages: HashMap<String, Package>,
     games: HashMap<String, Composition>,
     theorems: HashMap<String, Theorem<'a>>,
 }
@@ -112,6 +113,7 @@ impl<'a> Project<'a> {
     pub(crate) fn empty() -> Self {
         Self {
             root_dir: PathBuf::new(),
+            packages: HashMap::new(),
             games: HashMap::new(),
             theorems: HashMap::new(),
         }
@@ -151,6 +153,7 @@ impl<'a> Project<'a> {
         let theorems = load::theorems(&files.theorems, packages.to_owned(), games.to_owned())?;
 
         let project = Project {
+            packages,
             root_dir,
             games,
             theorems,
@@ -264,6 +267,30 @@ impl<'a> Project<'a> {
             }
 
             ui.finish_theorem(&theorem.name);
+        }
+
+        Ok(())
+    }
+
+    pub fn python(&self) -> Result<()> {
+        let mut path = self.root_dir.clone();
+        path.push("_build/python/");
+        std::fs::create_dir_all(&path)?;
+
+        println!("from dataclasses import dataclass");
+
+        for (name, pkg) in &self.packages {
+            use crate::writers::python::patterns::game_state::*;
+
+            println!("{}", DataclassWriter(PackageStatePattern::new(pkg)));
+        }
+
+        for (name, proof) in &self.theorems {
+            for game_inst in &proof.instances {
+                use crate::writers::python::patterns::game_state::*;
+
+                println!("{}", DataclassWriter(GameStatePattern::new(game_inst)));
+            }
         }
 
         Ok(())

src/writers/python/patterns/game_state.rs
@@ -1,0 +1,165 @@
+/*
+*
+*
+* We need to
+* - figure out the types to use on python's side
+*   - bits_n
+*     - BitVector? external dep.
+*     - bitvec? same problem.
+*     - maybe we just need deps
+*   - int -> int
+*   - bool -> bool
+*
+* - we need the const assignments in here - maybe simply full gameinstance?
+* - what sample info do we need?
+*   - here, not so much, but we need to find a way to do randomness mapping
+*
+* */
+
+use std::fmt::Display;
+
+use crate::{package::Package, theorem::GameInstance};
+
+static UNIT: () = ();
+
+pub struct GameStatePattern<'a> {
+    game_inst: &'a GameInstance,
+}
+
+impl<'a> GameStatePattern<'a> {
+    pub fn new(game_inst: &'a GameInstance) -> Self {
+        Self { game_inst }
+    }
+}
+
+pub struct PackageStatePattern<'a> {
+    pkg: &'a Package,
+}
+
+impl<'a> PackageStatePattern<'a> {
+    pub fn new(pkg: &'a Package) -> Self {
+        Self { pkg }
+    }
+}
+
+struct GameStateTypeName<'a>(&'a str);
+enum GameStateFieldName<'a> {
+    PackageState(&'a str),
+    Randomness(&'a str),
+}
+struct PackageStateTypeName<'a>(&'a str);
+struct PackageStateFieldName<'a>(&'a str);
+
+impl<'a> Display for GameStateTypeName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self(name) = self;
+        write!(f, "GameState_{name}")
+    }
+}
+
+impl<'a> Display for GameStateFieldName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            GameStateFieldName::PackageState(name) => write!(f, "pkg_{name}"),
+            GameStateFieldName::Randomness(_) => todo!(),
+        }
+    }
+}
+
+impl<'a> Display for PackageStateTypeName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self(name) = self;
+        write!(f, "PackageState_{name}")
+    }
+}
+
+impl<'a> Display for PackageStateFieldName<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self(name) = self;
+        write!(f, "{name}")
+    }
+}
+
+impl<'a> Dataclass for PackageStatePattern<'a> {
+    fn name(&self) -> impl Display {
+        PackageStateTypeName(self.pkg.name())
+    }
+
+    fn fields(&self) -> impl IntoIterator<Item = (impl Display, PyType<'_>)> {
+        self.pkg.state.iter().map(|(name, ty, _)| {
+            (
+                PackageStateFieldName(name.as_str()),
+                ty.clone().try_into().unwrap(),
+            )
+        })
+    }
+}
+
+impl<'a> Dataclass for GameStatePattern<'a> {
+    fn name(&self) -> impl Display {
+        self.game_inst.game().name()
+    }
+
+    fn fields(&self) -> impl IntoIterator<Item = (impl Display, PyType)> {
+        self.game_inst.game.pkgs.iter().map(|pkg_inst| {
+            (
+                GameStateFieldName::PackageState(pkg_inst.name()),
+                PyType::PackageState(pkg_inst.pkg_name()),
+            )
+        })
+    }
+}
+
+pub struct DataclassWriter<DC: Dataclass>(pub DC);
+
+impl<DC: Dataclass> Display for DataclassWriter<DC> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "@dataclass")?;
+        writeln!(f, "class {}:", self.0.name())?;
+        for (name, ty) in self.0.fields() {
+            writeln!(f, "  {name}: {ty}")?;
+        }
+        Ok(())
+    }
+}
+
+struct BitVecLength<'a>(&'a ());
+
+enum PyType<'a> {
+    BitVec(BitVecLength<'a>),
+    Int,
+    Bool,
+    List(Box<Self>),
+    PackageState(&'a str),
+}
+
+impl<'a> Display for PyType<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PyType::BitVec(bit_vec_length) => todo!(),
+            PyType::Int => write!(f, "int"),
+            PyType::Bool => write!(f, "bool"),
+            PyType::List(py_type) => write!(f, "List[{py_type}]"),
+            PyType::PackageState(name) => write!(f, "{}", PackageStateTypeName(name)),
+        }
+    }
+}
+
+impl<'a> TryFrom<crate::types::Type> for PyType<'a> {
+    type Error = ();
+
+    fn try_from(value: crate::types::Type) -> Result<Self, Self::Error> {
+        match value {
+            crate::types::Type::Integer => Ok(PyType::Int),
+            crate::types::Type::Boolean => Ok(PyType::Bool),
+            crate::types::Type::Bits(_count_spec) => Ok(PyType::BitVec(BitVecLength(&UNIT))),
+            crate::types::Type::List(ty) => PyType::try_from(*ty).map(Box::new).map(PyType::List),
+            _ => todo!(),
+        }
+    }
+}
+
+trait Dataclass {
+    fn name(&self) -> impl Display;
+    fn fields(&self) -> impl IntoIterator<Item = (impl Display, PyType)>;
+}