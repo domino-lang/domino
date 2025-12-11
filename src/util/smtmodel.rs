9cf9d3e5669b89e66599a68c0b0c588f6516f714
Christoph Egger <egger@irif.fr>
2025-12-11 01:40:09 +0100

    basic model parsing

--- src/util/smtmodel.rs
+++ src/util/smtmodel.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use crate::debug_assert_matches;
+use crate::writers::smt::exprs::SmtExpr;
 
 use pest::Parser;
 extern crate pest;
@@ -36,20 +37,34 @@ impl SmtModelParser {
 
 #[derive(Debug, Clone)]
 pub enum SmtModelEntry {
-    IntEntry { name: String, value: i32 },
+    IntEntry {
+        name: String,
+        value: i32,
+    },
+    BoolEntry {
+        name: String,
+        value: bool,
+    },
+    UnknownEntry {
+        name: String,
+        value: SmtExpr,
+        ty: String,
+    },
 }
 
 impl SmtModelEntry {
     pub fn name(&self) -> &str {
         match &self {
             SmtModelEntry::IntEntry { name, .. } => name,
+            SmtModelEntry::BoolEntry { name, .. } => name,
+            SmtModelEntry::UnknownEntry { name, .. } => name,
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct SmtModel {
-    values: Vec<SmtModelEntry>,
+    pub(super) values: Vec<SmtModelEntry>,
 }
 
 impl SmtModel {
@@ -75,5 +90,20 @@ impl SmtModel {
             .iter()
             .find(|entry| entry.name() == name)
             .cloned()
+    }
+
+    pub fn get_value_as_int(&self, name: &str) -> Option<i32> {
+        if let Some(SmtModelEntry::IntEntry { value, .. }) = self.get_value(name) {
+            Some(value)
+        } else {
+            None
+        }
+    }
+    pub fn get_value_as_bool(&self, name: &str) -> Option<bool> {
+        if let Some(SmtModelEntry::BoolEntry { value, .. }) = self.get_value(name) {
+            Some(value)
+        } else {
+            None
+        }
     }
 }
---
+++ src/util/smtparser/test.rs
@@ -0,0 +1,27 @@
+use super::parse_model;
+
+#[test]
+fn parse_model_files() {
+    let file = std::fs::File::open("src/util/smtparser/testdata/model.smt2").unwrap();
+    let content = std::io::read_to_string(file).unwrap();
+
+    let (model, length) = parse_model(&content);
+    dbg!(&model);
+
+    assert_eq!(length, 3209);
+    assert_eq!(
+        model.get_value_as_int("randctr-small_composition-0"),
+        Some(0)
+    );
+    assert_eq!(model.get_value_as_bool("<equal-aborts>"), Some(true));
+}
+
+#[test]
+fn only_parses_one_model() {
+    let file = std::fs::File::open("src/util/smtparser/testdata/model_double.smt2").unwrap();
+    let content = std::io::read_to_string(file).unwrap();
+
+    let (model, length) = parse_model(&content);
+
+    assert_eq!(length, 3209);
+}
