ffdfc66e12a41f5813ec4ed6777cd9510ff842fb
Christoph Egger <egger@irif.fr>
2025-12-11 23:10:49 +0100

    add tests

--- src/proof.rs
+++ src/proof.rs
@@ -434,3 +434,72 @@ impl std::fmt::Display for Proof<'_> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+
+    use crate::parser::{
+        composition::handle_composition, package::handle_pkg, theorem::handle_theorem, SspParser,
+    };
+
+    // Bug 196,
+    // assignment of incompatible literals
+    #[test]
+    fn bug192_should_fail_incompatible_assing() {
+        let pkgstring = "package Pkg {params {a: Bool, b: Bool,} state {}}";
+        let mut pkgast = SspParser::parse_package(pkgstring).unwrap();
+        let pkg = handle_pkg("test", pkgstring, pkgast.next().unwrap()).unwrap();
+
+        let mut pkg_map = HashMap::new();
+        pkg_map.insert(pkg.0, pkg.1);
+
+        let compstring = "composition Comp {const a: Bool; const b: Bool;}";
+        let mut compast = SspParser::parse_composition(compstring).unwrap();
+        let comp =
+            handle_composition("test", compstring, compast.next().unwrap(), &pkg_map).unwrap();
+
+        let mut comp_map = HashMap::new();
+        comp_map.insert("Comp".to_string(), comp);
+
+        let thmstring = "theorem Thm {const b: Bool; \
+                         instance Ia = Comp {params{ a:true,b:false }} \
+                         instance Ib = Comp {params{ a:b,b:b }} \
+                         gamehops {}}";
+        let mut thmast = SspParser::parse_theorem(thmstring).unwrap();
+        let thm =
+            handle_theorem("test", thmstring, thmast.next().unwrap(), pkg_map, comp_map).unwrap();
+
+        let [ref instance_a, ref instance_b] = thm.instances[..] else {
+            unreachable!()
+        };
+
+        assert!(!super::game_is_compatible(instance_a, instance_b));
+    }
+    #[test]
+    fn bug192_should_succeed_compatible_assignment() {
+        let pkg_map = HashMap::new();
+
+        let compstring = "composition Comp {const a: Bool; const b: Bool;}";
+        let mut compast = SspParser::parse_composition(compstring).unwrap();
+        let comp =
+            handle_composition("test", compstring, compast.next().unwrap(), &pkg_map).unwrap();
+
+        let mut comp_map = HashMap::new();
+        comp_map.insert("Comp".to_string(), comp);
+
+        let thmstring = "theorem Thm {const b: Bool; \
+                         instance Ia = Comp {params{ a:true,b:true }} \
+                         instance Ib = Comp {params{ a:b,b:b }} \
+                         gamehops {}}";
+        let mut thmast = SspParser::parse_theorem(thmstring).unwrap();
+        let thm =
+            handle_theorem("test", thmstring, thmast.next().unwrap(), pkg_map, comp_map).unwrap();
+
+        let [ref instance_a, ref instance_b] = thm.instances[..] else {
+            unreachable!()
+        };
+
+        assert!(super::game_is_compatible(instance_a, instance_b));
+    }
+}
