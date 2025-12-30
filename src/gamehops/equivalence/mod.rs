a4811c70cc453b8f65f2a6851f31f190cb250cdc
Christoph Egger <egger@irif.fr>
2025-12-30 14:39:58 +0100

    Add a smt parser based linting step to equivalence proofs

--- src/gamehops/equivalence/mod.rs
+++ src/gamehops/equivalence/mod.rs
@@ -112,6 +112,7 @@ impl Equivalence {
 }
 
 pub mod error;
+mod lint;
 mod verify_fn;
 
 use error::{Error, Result};
@@ -783,6 +784,8 @@ impl<'a> EquivalenceContext<'a> {
     }
 
     fn emit_invariant(&self, comm: &mut Communicator, oracle_name: &str) -> Result<()> {
+        let mut linter = lint::Linter::new(self, oracle_name);
+
         for file_name in &self.equivalence.invariants_by_oracle_name(oracle_name) {
             log::info!("reading file {file_name}");
             let file_contents = std::fs::read_to_string(file_name).map_err(|err| {
@@ -790,28 +793,7 @@ impl<'a> EquivalenceContext<'a> {
                 error::new_invariant_file_read_error(oracle_name.to_string(), file_name, err)
             })?;
             log::info!("read file {file_name}");
-
-            let parsed_sampleids = crate::util::smtparser::extract_sampleid(&file_contents);
-            let left_sample_pos = &self.sample_info_left().positions;
-            let right_sample_pos = &self.sample_info_right().positions;
-
-            for sampleinfo in &parsed_sampleids {
-                if left_sample_pos.iter().any(|samplepos| {
-                    samplepos.inst_name == sampleinfo.pkgname
-                        && samplepos.oracle_name == sampleinfo.oraclename
-                        && samplepos.sample_name == sampleinfo.samplename
-                }) && right_sample_pos.iter().any(|samplepos| {
-                    samplepos.inst_name == sampleinfo.pkgname
-                        && samplepos.oracle_name == sampleinfo.oraclename
-                        && samplepos.sample_name == sampleinfo.samplename
-                }) {
-                    let smtfun = &sampleinfo.smtfunname;
-                    log::warn!("Invariant code in {file_name} uses {sampleinfo} in function {smtfun} which does not occur in package code");
-                }
-            }
-
-            dbg!(parsed_sampleids);
-
+            linter.lint_file(file_name, &file_contents)?;
             write!(comm, "{file_contents}").unwrap();
             log::info!("wrote contents of file {file_name}");
 
@@ -822,6 +804,7 @@ impl<'a> EquivalenceContext<'a> {
                 });
             }
         }
+        linter.lint_finish()?;
 
         Ok(())
     }
--- src/util/smtparser/mod.rs
+++ src/util/smtparser/mod.rs
@@ -2,12 +2,16 @@
 
 mod implementation;
 
+mod functions;
 mod model;
 mod sampleid;
 
+pub use functions::extract as extract_functions;
 use implementation::SmtParser;
 pub use model::parse as parse_model;
 pub use sampleid::extract as extract_sampleid;
+
+pub use functions::ExtractedFunction;
 
 #[cfg(test)]
 mod test;
