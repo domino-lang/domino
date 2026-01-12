43c35e0e31cb7367bbed7f646c5e1c84adcc6652
Christoph Egger <egger@irif.fr>
2026-01-12 16:11:37 +0100

    Update static website for incremental cvc
    To use build cvc5 with
    
    ```
    ./configure.sh --static --static-binary --auto-download --wasm=JS --wasm-flags='-s EXPORTED_FUNCTIONS=_main -s MODULARIZE=1 -s EXPORT_NAME=Cvc5Module -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=1 -s INVOKE_RUN=0 -s EXPORT_ES6=1 -s ENVIRONMENT=web,worker -s EXPORTED_RUNTIME_METHODS=getValue'
    ```
    
    and then update in the `CMakeCache.txt` to
    
    ```
    WASM_FLAGS:STRING=-s EXPORTED_FUNCTIONS=_main,_cvc5_parser_new,_cvc5_parser_set_inc_str_input,_cvc5_parser_append_inc_str_input,_cvc5_parser_next_command,_cvc5_new,_cvc5_cmd_invoke,_cvc5_symbol_manager_new,_cvc5_term_manager_new,_cvc5_term_manager_new,_cvc5_parser_done,_cvc5_check_sat,_cvc5_get_model,_cvc5_result_to_string,_cvc5_result_is_sat,_cvc5_result_is_unsat,_cvc5_result_is_unknown,_cvc5_result_release,_malloc,_free -s MODULARIZE=1 -s EXPORT_NAME=Cvc5Module -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=1 -s INVOKE_RUN=0 -s EXPORT_ES6=1 -s ENVIRONMENT=web,worker -s EXPORTED_RUNTIME_METHODS=callMain,FS,run,UTF8ToString,cwrap,ccall,setValue,getValue
    ```

--- crates/domino-web/static/domino-worker.js
+++ crates/domino-web/static/domino-worker.js
@@ -1,10 +1,81 @@
 import init, { helloworld, proofsteps, prove } from './pkg/domino_web.js';
+import Cvc5Module from "./cvc5/cvc5.js";
 
-async function init_domino() {
-    await init()
+
+
+class Solver {
+    constructor(cvc) {
+        this.cvc = cvc;
+        this.tm = cvc._term_manager_new();
+        this.solver = cvc._new(this.tm);
+        this.sm = cvc._symbol_manager_new(this.tm);
+        this.parser = cvc._parser_new(this.solver, this.sm);
+        cvc._parser_set_inc_str_input(this.parser, 0, "domino");
+        this.add_smt('(set-logic ALL)');
+        this.add_smt('(set-option :produce-models true)');
+        this.add_smt('(set-option :incremental true)');
+    }
+
+    add_smt(smt) {
+        const cvc = this.cvc;
+        cvc._parser_append_inc_str_input(this.parser, smt);
+    }
+
+    check_sat() {
+        const cvc = this.cvc;
+        const err = cvc._malloc(4);
+        while (! cvc._parser_done(this.parser)) {
+            const cmd = cvc._parser_next_command(this.parser, err);
+            if (cmd == 0) break;
+            const out = cvc._cmd_invoke(cmd, this.solver, this.sm);
+            console.log(out);
+        }
+        const result = cvc._check_sat(this.solver);
+        const ret = cvc._result_to_string(result);
+        cvc._result_release(result);
+        cvc._parser_set_inc_str_input(this.parser, 0, "domino");
+        return ret;
+    }
+
+    get_model() {
+        const cvc = this.cvc;
+        const err = cvc._malloc(4);
+        cvc._parser_append_inc_str_input(this.parser, "(get-model)")
+        const cmd = cvc._parser_next_command(this.parser, err);
+        const out = cvc._cmd_invoke(cmd, this.solver, this.sm);
+        return out;
+    }
 }
 
-init_domino();
+
+class CVC {
+    constructor(module) {
+        this.module = module;
+
+        this._term_manager_new = module.cwrap('cvc5_term_manager_new', 'number', []);
+        this._new = module.cwrap('cvc5_new', 'number', ['number']);
+        this._symbol_manager_new = module.cwrap('cvc5_symbol_manager_new', 'number', ['number']);
+        this._parser_new = module.cwrap('cvc5_parser_new', 'number', ['number', 'number']);
+        this._parser_set_inc_str_input = module.cwrap('cvc5_parser_set_inc_str_input', null, ['number', 'number', 'string']);
+        this._parser_append_inc_str_input = module.cwrap('cvc5_parser_append_inc_str_input', null, ['number', 'string']);
+        this._parser_next_command = module.cwrap('cvc5_parser_next_command', 'number', ['number', 'number']);
+        this._parser_done = module.cwrap('cvc5_parser_done', 'number', ['number']);
+        this._cmd_invoke = module.cwrap('cvc5_cmd_invoke', 'string', ['number', 'number', 'number']);
+        this._check_sat = module.cwrap('cvc5_check_sat', 'number', ['number']);
+        this._result_to_string = module.cwrap('cvc5_result_to_string', 'string', ['number']);
+        this._result_is_sat = module.cwrap('cvc5_result_is_sat', 'number', ['number']);
+        this._result_is_unsat = module.cwrap('cvc5_result_is_unsat', 'number', ['number']);
+        this._result_is_unknown = module.cwrap('cvc5_result_is_unknown', 'number', ['number']);
+        this._result_release = module.cwrap('cvc5_result_release', null, ['number']);
+
+        this._malloc = module.cwrap('malloc', 'number', ['number']);
+        this._free = module.cwrap('free', null, ['number']);
+    }
+}
+
+
+let cvc;
+
 
 onmessage = (e) => {
     console.log(e.data.func, e.data);
@@ -12,6 +83,16 @@ onmessage = (e) => {
     case "proofsteps":
         console.log("proofsteps");
         postMessage({func: "proofsteps", data: proofsteps(e.data.data), filename: e.data.filename});
+        break
+    case "cvctest":
+        const solver = new Solver(cvc);
+        solver.add_smt("(declare-const foo Int)");
+        solver.add_smt("(declare-const bar Int)");
+        console.log(solver.check_sat());
+        postMessage({func: "cvc-solve", data: solver.get_model()});
+        solver.add_smt("(assert (< foo bar))");
+        console.log(solver.check_sat());
+        postMessage({func: "cvc-solve", data: solver.get_model()});
         break;
     case "prove":
         console.log("prove start");
@@ -22,21 +103,20 @@ onmessage = (e) => {
 }
 
 
-const cvcWorker = new Worker("cvc-worker.js");
-cvcWorker.onmessage = (e) => {
-    console.log(e);
-    postMessage(e.data);
+
+
+Cvc5Module({
+    locateFile: function(fname) {
+        return `cvc5/${fname}`;
+    },
+    print: function(text) {
+        postMessage({func: "cvc-solve", data: text});
+    },
+
+}).then(instance => {cvc = new CVC(instance)});
+
+async function init_domino() {
+    await init()
 }
-cvcWorker.postMessage("(set-logic ALL)(declare-const test Int)(check-sat)(get-model)")
 
-
-          // proofstep_button.addEventListener("click", function() {
-          //     let zipfile = document.getElementById("file_upload").files[0];
-          //     console.log(zipfile);
-          //     zipfile.bytes().then(function (bs) {
-          //         let output_element = document.getElementById("domino_output_element");
-          //         output_element.innerHTML = proofsteps(bs);
-          //     });
-          // });
-
-
+init_domino();
