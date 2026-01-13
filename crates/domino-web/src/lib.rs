e58f9acaf086a79a00c76c1ab6438e6582205cd6
Christoph Egger <egger@irif.fr>
2026-01-13 14:41:09 +0100

    test using cvc solver from domino binary

--- crates/domino-web/src/lib.rs
+++ crates/domino-web/src/lib.rs
@@ -1,8 +1,27 @@
 use std::io::Cursor;
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use sspverif::util::prover::process::ProverBackend;
+use sspverif::util::prover::ProverBackend;
 use web_sys::console::{error_1, log_1};
+
+#[wasm_bindgen(module = "/static/cvc.js")]
+extern "C" {
+    type CVC;
+
+    type Solver;
+
+    #[wasm_bindgen(constructor)]
+    fn new(module: &CVC) -> Solver;
+
+    #[wasm_bindgen(method)]
+    fn check_sat(this: &Solver) -> String;
+
+    #[wasm_bindgen(method)]
+    fn get_model(this: &Solver) -> String;
+
+    #[wasm_bindgen(method)]
+    fn add_smt(this: &Solver, content: &str);
+}
 
 #[wasm_bindgen]
 pub fn proofsteps(zipfile: &[u8]) -> String {
@@ -42,4 +61,19 @@ pub fn prove(zipfile: &[u8]) {
 #[wasm_bindgen]
 pub fn helloworld() -> String {
     "Hello World".to_string()
+}
+
+#[wasm_bindgen]
+pub fn test(module: &CVC) -> String {
+    let solver = Solver::new(module);
+
+    solver.add_smt("(declare-const a Int)");
+    solver.add_smt("(declare-const b Int)");
+    solver.add_smt("(declare-const c Int)");
+    solver.add_smt("(declare-const d Int)");
+    solver.add_smt("(assert (< a b c d))");
+    solver.add_smt("(assert (= a (* 5 d)))");
+    solver.add_smt("(assert (= b (* 3 c)))");
+    solver.check_sat();
+    solver.get_model()
 }
