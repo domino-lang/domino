8326b685889fd8f0e51eec8e86888690b681991b
Jan Winkelmann (keks) <jan@cryspen.com>
2026-02-19 08:28:24 +0100

    the vibe-coded rest (probably buggy)

--- src/writers/smt/sorts.rs
+++ src/writers/smt/sorts.rs
@@ -60,6 +60,7 @@ impl From<&Type> for SmtlibSort {
             },
 
             TypeKind::UserDefined(_) => todo!(),
+            TypeKind::TypeParam(_) => unreachable!("TypeParam should be fully instantiated before reaching the SMT backend"),
             TypeKind::Set(_) => todo!(),
             TypeKind::List(_) => todo!(),
             TypeKind::Fn(_, _) => todo!(),
@@ -120,6 +121,7 @@ impl From<Type> for Sort {
             TypeKind::Set(_) => todo!(),
             TypeKind::Fn(_, _) => todo!(),
             TypeKind::UserDefined(_) => todo!(),
+            TypeKind::TypeParam(_) => unreachable!("TypeParam should be fully instantiated before reaching the SMT backend"),
         }
     }
 }
