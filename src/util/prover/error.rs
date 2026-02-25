b8d9dd79133fa18f563bc233ea32680d3b1cb081
Christoph Egger <egger@irif.fr>
2026-02-25 22:57:44 +0100

    put subprocess stuff behind a feature

--- src/util/prover/error.rs
+++ src/util/prover/error.rs
@@ -6,6 +6,7 @@ pub enum Error {
     WriteError(#[from] std::fmt::Error),
     #[error("io error: {0}")]
     IOError(#[from] std::io::Error),
+    #[cfg(feature = "process-solver")]
     #[error("error interacting with prover process: {0}")]
     ProcessError(#[from] crate::util::process::Error),
     #[error("prover error: {0}")]
