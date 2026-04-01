e01e0690c353599eaa16f8b461701ddc7e3f2396
Jan Winkelmann (keks) <jan@cryspen.com>
2026-04-01 23:50:58 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,136 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.extend fenix.overlays.default;
+
+        dominoToolchain = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "rustc"
+          "clippy"
+          "rustfmt"
+          "rust-std"
+        ];
+
+        toml = pkgs.lib.importTOML ./crates/domino/Cargo.toml;
+        workspaceToml = pkgs.lib.importTOML ./Cargo.toml;
+
+        src = pkgs.lib.sources.sourceByRegex ./. [
+          "^Cargo.(lock|toml)$"
+          "^crates$"
+          "^crates/.*"
+          "^src$"
+          "^src/.*"
+          "^testdata$"
+          "^testdata/.*"
+          "^example-projects$"
+          "^example-projects/.*"
+          "^scripts"
+          "^scripts/.*"
+        ];
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = dominoToolchain;
+          rustc = dominoToolchain;
+        };
+
+        domino = rustPlatform.buildRustPackage {
+          name = toml.package.name;
+          version = workspaceToml.workspace.package.version;
+          src = src;
+          doCheck = true;
+          cargoLock.lockFile = ./Cargo.lock;
+          buildAndTestSubdir = "crates/domino/";
+          cargoBuildFlags = [ "--workspace" ];
+          checkPhase = ''
+            export RUSTFLAGS="-D warnings"
+            cargo test --verbose --workspace --all-targets
+            cargo test --verbose --workspace --doc
+            cargo check --verbose --workspace --all-targets
+            cargo clippy --verbose --workspace --all-targets -- --deny warnings
+            cargo fmt --all --verbose --check
+          '';
+          meta.license = with pkgs.lib.licenses; [
+            mit
+            asl20
+          ];
+          meta.platforms = pkgs.lib.platforms.all;
+        };
+
+        dominoTreefmt = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.nixfmt.enable = true;
+          programs.rustfmt.enable = true;
+          programs.rustfmt.edition = workspaceToml.workspace.package.edition;
+          programs.prettier.enable = true;
+          programs.taplo.enable = true;
+          programs.taplo.excludes = [
+            "**/ssp.toml"
+            # skip this one - it is very old and uses an outdated syntax. probably should delete this.
+            "example-projects/yao"
+          ];
+          programs.shfmt.enable = true;
+          programs.prettier.includes = [
+            "*.md"
+            "*.yaml"
+            "*.yml"
+          ];
+        };
+
+        devShellPackages = [
+          dominoToolchain
+          pkgs.cargo-release
+          pkgs.rustfmt
+        ];
+      in
+      {
+        packages.default = domino;
+        packages.domino = domino;
+        packages.dominoToolchain = dominoToolchain;
+
+        formatter = dominoTreefmt.config.build.wrapper;
+
+        checks.default = domino;
+        checks.domino = domino;
+        checks.treefmt = dominoTreefmt.config.build.check self;
+        checks.knownWorkingExamples = pkgs.stdenv.mkDerivation {
+          name = "domino-knownWorkingExamples";
+          src = src;
+          buildInputs = with pkgs; [
+            domino
+            cvc5
+          ];
+          buildCommand = ''
+            cp -R "$src" src/
+            chmod -R u+w src/
+            cd src
+            DOMINO="$(type -p domino)" ${pkgs.bash}/bin/bash ./scripts/test-known-examples.sh
+            touch $out
+          '';
+        };
+
+        devShells.default = pkgs.mkShellNoCC {
+          packages = devShellPackages ++ [ domino ];
+        };
+        devShells.dev = pkgs.mkShellNoCC {
+          packages = devShellPackages;
+        };
+      }
+    );
+}
