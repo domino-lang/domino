6bbdc7437df95658c579408a012a473350d0fe58
Jan Winkelmann <146678+keks@users.noreply.github.com>
2026-04-15 21:44:07 +0200

    Add Nix Flake for running Domino (#295)
    Add a flake.nix based on @koraa's template. The flake contains a domino package and devshell, as well as some auxiliary tools for formatting the code in this Repo. Formatting Domino code is currently not supported, because our parser destroys comments. This is an open issue. Once that is addressed, the functionality will be added to the flake as well.

--- scripts/test-known-examples.sh
+++ scripts/test-known-examples.sh
@@ -3,67 +3,66 @@
 failed_path=$(realpath .)/failed
 
 function fail() {
-    echo $1 >&2
-    touch $failed_path
+  echo $1 >&2
+  touch $failed_path
 }
 
 DOMINO=$(realpath $DOMINO)
 
 echo "# Checking Test Projects"
 for project_path in test-projects/*; do
-    project=$(basename $project_path)
-    (
-        echo "## Checking $project ..."
-        cd $project_path
-        if [[ "$project" == "err-"* ]]; then
-            $DOMINO prove && fail "expected error, but succeeded: $project_path"
-        else
-            $DOMINO prove || fail "expected success, but failed: $project_path"
-        fi
-    )
+  project=$(basename $project_path)
+  (
+    echo "## Checking $project ..."
+    cd $project_path
+    if [[ $project == "err-"* ]]; then
+      $DOMINO prove && fail "expected error, but succeeded: $project_path"
+    else
+      $DOMINO prove || fail "expected success, but failed: $project_path"
+    fi
+  )
 done
 
 # sed script from https://unix.stackexchange.com/a/244479
 echo "# Checking known-good proofs still prove..."
 sed -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' "example-projects/known-good.txt" | while read project; do
-    project_path=example-projects/$project
-    # skip comments
-    if [ ! -d $project_path ]; then
-        echo "WARN: skipping non-existing project in known-good proofs: $project"
-        continue
-    fi
+  project_path=example-projects/$project
+  # skip comments
+  if [ ! -d $project_path ]; then
+    echo "WARN: skipping non-existing project in known-good proofs: $project"
+    continue
+  fi
+  (
+    echo "## Checking $project proves..."
+    cd $project_path
+    $DOMINO prove || fail "expected success, but failed: $project_path"
+    echo "## Checking $project latex export..."
+    $DOMINO latex || fail "expected success, but failed: $project_path"
     (
-        echo "## Checking $project proves..."
-        cd $project_path
-        $DOMINO prove || fail "expected success, but failed: $project_path"
-        echo "## Checking $project latex export..."
-        $DOMINO latex || fail "expected success, but failed: $project_path"
-        (
-            cd _build/latex
-            for file in Theorem_*.tex; do
-                TEMPFILE=`mktemp`
-                pdflatex "$file" >$TEMPFILE 2>&1 || fail "$(tail -n 20 $TEMPFILE)"
-            done
-        )
+      cd _build/latex
+      for file in Theorem_*.tex; do
+        TEMPFILE=$(mktemp)
+        pdflatex "$file" >$TEMPFILE 2>&1 || fail "$(tail -n 20 $TEMPFILE)"
+      done
     )
+  )
 done
 
 echo "# Checking slow known-good proofs still parse..."
 sed -e 's/[[:space:]]*#.*// ; /^[[:space:]]*$/d' "example-projects/known-good-slow.txt" | while read project; do
-    project_path=example-projects/$project
-    # skip comments
-    if [ ! -d $project_path ]; then
-        echo "WARN: skipping non-existing project in known-good proofs: $project"
-        continue
-    fi
-    (
-        echo "## Checking $project parses..."
-        cd $project_path
-        $DOMINO proofsteps || fail "expected success, but failed: $project_path"
-    )
+  project_path=example-projects/$project
+  # skip comments
+  if [ ! -d $project_path ]; then
+    echo "WARN: skipping non-existing project in known-good proofs: $project"
+    continue
+  fi
+  (
+    echo "## Checking $project parses..."
+    cd $project_path
+    $DOMINO proofsteps || fail "expected success, but failed: $project_path"
+  )
 done
 
-
 if [ -f $failed_path ]; then
-    exit 1
+  exit 1
 fi
