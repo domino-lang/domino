95dbd4f2b2702615285d6695e0d4ecf85b47c518
Christoph Egger <egger@irif.fr>
2026-08-06 15:57:42 +0200

    multiple package / game invariants

--- src/gamehops/equivalence/smtclaim.rs
+++ src/gamehops/equivalence/smtclaim.rs
@@ -14,6 +14,7 @@ pub struct CustomSmtWarning {
 pub enum ClaimType {
     RawSmt,
     Function,
+    //Randomness,
     Lemma,
     Relation,
     Invariant,
@@ -76,31 +77,30 @@ impl SmtClaim {
         }
     }
 
-    pub fn new_package_invariant(
-        ty: ClaimType,
-        smt: SmtExpr,
-        game_name: &str,
-        pkg_name: &str,
-        filename: String,
-    ) -> Self {
+    pub fn new_package_invariant(ty: ClaimType, smt: SmtExpr, filename: String) -> Self {
+        let SmtExpr::List(list) = &smt else { todo!() };
+        let SmtExpr::Atom(ref name) = list[1] else {
+            todo!()
+        };
+        let name = name.to_string();
         Self {
             ty,
             smt,
-            name: format!("package-invariant!{game_name}-{pkg_name}!"),
+            name,
             filename,
         }
     }
 
-    pub fn new_game_invariant(
-        ty: ClaimType,
-        smt: SmtExpr,
-        game_name: &str,
-        filename: String,
-    ) -> Self {
+    pub fn new_game_invariant(ty: ClaimType, smt: SmtExpr, filename: String) -> Self {
+        let SmtExpr::List(list) = &smt else { todo!() };
+        let SmtExpr::Atom(ref name) = list[1] else {
+            todo!()
+        };
+        let name = name.to_string();
         Self {
             ty,
             smt,
-            name: format!("game-invariant!{game_name}!"),
+            name,
             filename,
         }
     }
